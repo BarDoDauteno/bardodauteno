419dd05faa0b20aefa03729f260d02b5ddaf5dbe
VitorJs00 <161177573+VitorJs00@users.noreply.github.com>
2025-10-01 12:30:24 -0300

    teste de comit

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,6 +4,7 @@ import PostCard from "./PostCard";
 const posts = [
     { title: "Resenha Da Semana", excerpt: "Como Marcos Vinicios Foi Mogado no dia 30/09/2025" },
     { title: "Dupla MVP", excerpt: "Neto&Kadafi Humilham beta no x1" },
+    { title: "Teste Comit", excerpt: "------" },
 ];
 
 const PostList: React.FC = () => (
